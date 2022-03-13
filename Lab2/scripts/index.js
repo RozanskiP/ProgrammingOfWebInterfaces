b9ffbfd2a33300bf29cf59bbc45d71f62754d012
Pawel <pawir4@wp.pl>
2022-03-13 11:24:36 +0100

    Added searching

--- Lab2/scripts/index.js
+++ Lab2/scripts/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let couterErrorMessages = 1;
-let couterOfIds = 0;
+let couterOfIds = 5;
 
 class Element {
   constructor(id, text, data, click) {
@@ -12,9 +12,20 @@ class Element {
   }
 }
 
-let list = [];
+let list = [
+            new Element(1,'one',null,true),
+            new Element(2,'two',null,true),
+            new Element(3,'thrEe',null,true),
+            new Element(4,'four',null,true)
+          ];
 let bin;
 
+// po zaladowaniu dokumentu narysuj przykladowa liste
+$(document).ready(function() {
+  draw();
+});
+
+// funkcja dodajaca nowy element
 const addNewElem = () => {
   const text = $("#new-elem").val();
   const listInHtml = document.getElementById("list");
@@ -40,24 +51,32 @@ const addNewElem = () => {
   }
 };
 
-const draw = () => {
+// funkcja rysujaca liste
+const draw = (shouldChangeList = false, newList = []) => {
   const listInHtml = document.getElementById("list");
   while (listInHtml.firstChild) {
     listInHtml.removeChild(listInHtml.firstChild);
   }
-  for (let i = 0; i < list.length; ++i) {
+
+  let copyOfList = list;
+
+  if(shouldChangeList){
+    copyOfList = newList;
+  }
+
+  for (let i = 0; i < copyOfList.length; ++i) {
     let newElem = document.createElement("li");
-    newElem.innerHTML = list[i].text;
-    newElem.id = list[i].id;
-    newElem.className = list[i].click
+    newElem.innerHTML = copyOfList[i].text;
+    newElem.id = copyOfList[i].id;
+    newElem.className = copyOfList[i].click
       ? "list-group-item list-group-item-primary"
       : "list-group-item list-group-item-secondary";
-    newElem.style = list[i].click
+    newElem.style = copyOfList[i].click
       ? "display: inline-block"
       : "text-decoration: line-through";
 
-    if (!list[i].click) {
-      newElem.innerHTML += list[i].data;
+    if (!copyOfList[i].click) {
+      newElem.innerHTML += copyOfList[i].data;
     }
     listInHtml.appendChild(newElem);
 
@@ -69,14 +88,15 @@ const draw = () => {
     let button = document.createElement("button");
     button.innerHTML = "X";
     button.className = "btn btn-warning";
-    button.id = list[i].id;
+    button.id = copyOfList[i].id;
     button.addEventListener("click", function (event) {
       var del = confirm("Are you sure to delete this todo?");
       if (del) {
-        bin = list.find((el) => el.id == event.target.id);
-        const index = list.indexOf(bin);
+        // id zostaje automatycznie przeksztalcony na Number przy ==
+        bin = copyOfList.find((el) => el.id == event.target.id);
+        const index = copyOfList.indexOf(bin);
         if (index > -1) {
-          list.splice(index, 1);
+          copyOfList.splice(index, 1);
         }
         $("#" + event.target.id).remove();
       }
@@ -85,6 +105,7 @@ const draw = () => {
   }
 };
 
+// event na dodanie nowego przedmiotu
 const listOfToDos = document.querySelector("ul");
 listOfToDos.addEventListener(
   "click",
@@ -119,6 +140,7 @@ listOfToDos.addEventListener(
   false
 );
 
+// przywrocenie ostatniej usunietej rzeczy
 const comebackWithLastElem = () => {
   if (bin !== null && bin !== undefined) {
     list.push(bin);
@@ -126,3 +148,39 @@ const comebackWithLastElem = () => {
     draw();
   }
 };
+
+// event przy wpisaniu litery do wyszukiwania
+const search = document.getElementById("search");
+search.addEventListener("keyup", function () {
+  searching();
+});
+// event na zmiane checkboxa
+const searchInChangeCheckbox = document.getElementById("search-with-case-sensitive");
+searchInChangeCheckbox.addEventListener("change", function () {
+  searching();
+});
+
+const searching = () => {
+  let filter = search.value;
+  if(search.value !== ""){
+    let copyOfList = [];
+    if(!document.getElementById("search-with-case-sensitive").checked){
+      filter = filter.toLowerCase();
+      for(let i = 0; i < list.length; ++i){
+        if(list[i].text.toLowerCase().indexOf(filter) > -1){
+          copyOfList.push(list[i]);
+        }
+      }
+    }else{
+      for(let i = 0; i < list.length; ++i){
+        if(list[i].text.indexOf(filter) > -1){
+          copyOfList.push(list[i]);
+        }
+      }
+    }
+    draw(true, copyOfList);
+  }else{
+    draw();
+  }
+}
+
